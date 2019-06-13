0490a691c6d173eb2cc14db7a11cd44582d1f3b4
Michael Chang <chang.michael1009@gmail.com>
2019-06-12 20:43:35 -0400

    Finished the experience section

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -5,6 +5,8 @@ import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import Fade from "react-reveal/Fade";
 import Pdf from "./resume/Resume_1Page_Michael_Chang.pdf";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 
 class Experience extends Component {
   render() {
@@ -23,14 +25,20 @@ class Experience extends Component {
               <Card.Body>
                 <Card.Title>Web Development</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  My solid foundation in web development comes from working on various projects to develop web applications.
+                  I have developed web applications using React JS, Angular, Spring, and Django and am comfortable learning new frameworks.
+                  In addition to these, I have also integrated web applications with Microsoft Azure and Travis CI.
                 </Card.Text>
                 <Card.Link
                   className="cardlink"
                   href="https://github.com/Michael1009/" target="_blank"
                 >
-                  <FontAwesomeIcon className="icon_blue" icon={["fab", "github"]} />
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={<Tooltip style={{ fontSize: "12px" }}>My projects!</Tooltip>}
+                  >
+                    <FontAwesomeIcon className="icon_blue" icon={["fab", "github"]} />
+                  </OverlayTrigger>
                 </Card.Link>
               </Card.Body>
             </Card>
@@ -44,11 +52,17 @@ class Experience extends Component {
               <Card.Body>
                 <Card.Title>Data Structures and Algorithms</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  My course work at UVA has solidified my understanding of data structures (CS2150) and algorithms (CS4102)
+                  by providing rigorous curriculums that pushed me to explore these topics thoroughly.
+                  I have to say that these courses were some of the most challenging, yet rewarding courses I've taken so far.
                 </Card.Text>
                 <Card.Link className="cardlink" href={Pdf} target="_blank">
-                  <FontAwesomeIcon className="icon_yellow" icon="file-alt" />
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={<Tooltip style={{ fontSize: "12px" }}>My education!</Tooltip>}
+                  >
+                    <FontAwesomeIcon className="icon_yellow" icon="file-alt" />
+                  </OverlayTrigger>
                 </Card.Link>
               </Card.Body>
             </Card>
@@ -62,20 +76,25 @@ class Experience extends Component {
               <Card.Body>
                 <Card.Title>Software Development</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
+                  Over the years, I've come to learn that software development is so much more than coding!
+                  My experience in the software development process comes from my personal experiences on software development teams (mostly using Agile) and an Advanced Software Development course at UVA.
                 </Card.Text>
                 <Card.Link
                   className="cardlink"
                   href="https://linkedin.com/in/mchang2017/" target="_blank"
                 >
-                  <FontAwesomeIcon className="icon_green" icon={["fab", "linkedin"]} />
+                  <OverlayTrigger
+                    placement="right"
+                    overlay={<Tooltip style={{ fontSize: "12px" }}>My experiences!</Tooltip>}
+                  >
+                    <FontAwesomeIcon className="icon_green" icon={["fab", "linkedin"]} />
+                  </OverlayTrigger>
                 </Card.Link>
               </Card.Body>
             </Card>
           </Fade>
-        </CardDeck>
-      </div>
+        </CardDeck >
+      </div >
     );
   }
 }
