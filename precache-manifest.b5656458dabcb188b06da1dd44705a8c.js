f23b745a3cd0036fbbc8891dcdb9a8953e59d75b
Michael Chang <chang.michael1009@gmail.com>
2019-06-13 20:29:14 -0400

    Updates

---
+++ precache-manifest.b5656458dabcb188b06da1dd44705a8c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6d5d71a6d6b5abcf549f96c917f119",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "3468a494a01f45cd5297",
+    "url": "/me/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6863ae49b57280cdfeff",
+    "url": "/me/static/css/main.8d33cf7f.chunk.css"
+  },
+  {
+    "revision": "3468a494a01f45cd5297",
+    "url": "/me/static/js/2.ff22b201.chunk.js"
+  },
+  {
+    "revision": "6863ae49b57280cdfeff",
+    "url": "/me/static/js/main.03a806c5.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  },
+  {
+    "revision": "39e38d074d50a234ecb2c9b5a9dbcb1e",
+    "url": "/me/static/media/Resume_1Page_Michael_Chang.39e38d07.pdf"
+  },
+  {
+    "revision": "9c66e343acef67a19ce79bb800341d3a",
+    "url": "/me/static/media/profilepic.9c66e343.jpg"
+  },
+  {
+    "revision": "a7e48e46ad8ec2374ff6f3afdffe8227",
+    "url": "/me/static/media/splash1.a7e48e46.jpeg"
+  },
+  {
+    "revision": "ededdb53fa4182d258f508e0335054e0",
+    "url": "/me/static/media/splash2.ededdb53.jpeg"
+  },
+  {
+    "revision": "0d3ed9275cd1f357f4204aa5d91d85bf",
+    "url": "/me/static/media/splash3.0d3ed927.jpeg"
+  },
+  {
+    "revision": "37adb6313c9928f96b9bebe8dfa309b0",
+    "url": "/me/static/media/splash4.37adb631.jpeg"
+  },
+  {
+    "revision": "f4ba531a86af71392ffac069fcd53d0a",
+    "url": "/me/static/media/splash5.f4ba531a.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.47c22711002f55ada5f83c1bfdc99f63.js"
+  "/me/precache-manifest.b5656458dabcb188b06da1dd44705a8c.js"
 );
 
 self.addEventListener('message', (event) => {
