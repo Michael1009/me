9e2a0b727733b19d9bee9843158e33c172c9e0b0
Michael1009 <chang.michael1009@gmail.com>
2019-06-19 15:45:11 -0400

    Starting to add links to the cards

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import "./css/projects.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
 import CardDeck from 'react-bootstrap/CardDeck'
 import Card from 'react-bootstrap/Card'
 import tearline_logo from './images/tearline.webp';
 import skibuilder_logo from './images/ski.png';
+import wired_logo from './images/wired_logo.png';
+
 
 class Projects extends Component {
 
@@ -21,14 +25,24 @@ class Projects extends Component {
             <Card.Body>
               <Card.Title>SkillMatch</Card.Title>
               <Card.Text>
-              SkiMatch is a web application that allows UVA students and faculty to connect over shared skills, 
-              interests, experiences and expertise. Users can create profiles that list their relevant academic information 
-              (i.e. major, past courses, UVA email, etc.) and skills, along with an explanation of why they are using the app 
-              (i.e. to find/provide tutoring, form study groups, or find community members with similar hobbies and interests).
+                SkiMatch is a web application that allows UVA students and faculty to connect over shared skills,
+                interests, experiences and expertise. Users can create profiles that list their relevant academic information
+                (i.e. major, past courses, UVA email, etc.) and skills, along with an explanation of why they are using the app
+                (i.e. to find/provide tutoring, form study groups, or find community members with similar hobbies and interests).
               </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <Card.Link
+                className="cardlink"
+                href="https://github.com/Michael1009/project-101-byteme" target="_blank"
+              >
+                <OverlayTrigger
+                  placement="right"
+                  overlay={<Tooltip style={{ fontSize: "12px" }}>SkillMatch</Tooltip>}
+                >
+                  <FontAwesomeIcon className="icon_blue" icon={["fab", "github"]} />
+                </OverlayTrigger>
+              </Card.Link>
             </Card.Footer>
           </Card>
           <Card>
@@ -36,12 +50,23 @@ class Projects extends Component {
             <Card.Body>
               <Card.Title>Tearline</Card.Title>
               <Card.Text>
-                This is a wider card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.
+                Created the Android Tearline Application for the National Geospatial-Intelligence Agency to promote the distribution of
+                unclassified intelligence. The team that I was assigned to, Pathfinder, had the goal of disseminating analysis on open source intelligence
+                and this application was one of the ways in how they did so.
               </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
+              <Card.Link
+                className="cardlink"
+                href="https://www.wired.com/2017/04/american-spies-now-smartphone-app/#slide-3" target="_blank"
+              >
+                <OverlayTrigger
+                  placement="right"
+                  overlay={<Tooltip style={{ fontSize: "12px" }}>WIRED Publication</Tooltip>}
+                >
+                  <img className="footer_logo" src={wired_logo} />
+                </OverlayTrigger>
+              </Card.Link>
             </Card.Footer>
           </Card>
 
